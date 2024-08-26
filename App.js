84dd83a50905e8fb5b22db0caf4b888046838dd9
sagarkataria <sagarsingh0110@gmail.com>
2024-08-26 11:31:17 +0530

    Create nested div by react

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
+/**
+ * <div id = "parent">
+ *      <div id = "child">
+ *        <h1>I'm h1 tag </h1>
+ *      </div>
+ * </div>
+ * 
+ * ReactElemet(Object) = HTML(Browser Underst)
+ */
 
- const heading =  React.createElement("h1",{id:'heading',xyz:"abc"},"Hello world from react");
- 
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(heading);
+const parent = React.createElement(
+    "div", { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, "I'm h1 tag"))
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
