3c9bfbae845ea93f70bd94089b1c30b38c236d38
sagarkataria <sagarsingh0110@gmail.com>
2024-08-26 12:55:59 +0530

    install react and react-dom and execute

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
-/**
- * <div id = "parent">
- *      <div id = "child">
- *        <h1>I'm h1 tag </h1>
- *        <h2>I'm h2 tag </h2>
- *      </div>
- *      <div id = "child2">
- *        <h1>I'm h1 tag </h1>
- *        <h2>I'm h2 tag </h2>
- *      </div>
- * </div>
- * 
- * ReactElemet(Object) = HTML(Browser Understands)
- */
+import React from 'react'
+import ReactDOM from 'react-dom';
 
 const parent = React.createElement(
     "div", { id: 'parent' }, [React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, "I'm h1 tag"), React.createElement('h2', {}, "I'm h2 tag")]),
--- README.md
+++ README.md
@@ -4,3 +4,8 @@
    - npm init
 - Most important package in react is bundler and in this project we are using parcel bundler
   - npm install -D parcel
+- Execute our react project need to install React and ReactDOM 
+  - npm install react
+  - npm install react-dom
+- Now we can execute our react project using parcel bundler 
+  - npx parcel index.html
