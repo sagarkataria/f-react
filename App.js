53754cd531dda174de1395c2588f925628ed35f8
sagarkataria <sagarsingh0110@gmail.com>
2024-08-26 11:32:52 +0530

    Create div and thier sibling h1 and h2 tags

--- App.js
+++ App.js
@@ -2,14 +2,15 @@
  * <div id = "parent">
  *      <div id = "child">
  *        <h1>I'm h1 tag </h1>
+ *        <h2>I'm h2 tag </h2>
  *      </div>
  * </div>
  * 
- * ReactElemet(Object) = HTML(Browser Underst)
+ * ReactElemet(Object) = HTML(Browser Understands)
  */
 
 const parent = React.createElement(
-    "div", { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, "I'm h1 tag"))
+    "div", { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, "I'm h1 tag"),React.createElement('h2',{},"I'm h2 tag")])
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
