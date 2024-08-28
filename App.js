39e757189b3dfa0d0f168c4563d1112776603dc9
sagarkataria <sagarsingh0110@gmail.com>
2024-08-28 23:32:15 +0530

    show restaurant card detailed with real data

--- App.js
+++ App.js
@@ -19,21 +19,106 @@ const Header = () => {
     )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const {resData} = props;
     return (
 
-        <div className='res-card' style={{ backgroundColor:"#f0f0f0" }}>
-        <img 
-        className='res-logo'
-         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/9/4373cf41-dc90-45b2-970a-0b16e4e3836c_570254.jpg"
-          alt=""/>
-          <div className=''>
-            <h3>Meghana food</h3>
-            <h4>Biryani ,North Indian,</h4>
-          </div>
+        <div className='res-card' style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className='res-logo'
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/9/4373cf41-dc90-45b2-970a-0b16e4e3836c_570254.jpg"
+                alt="" />
+            <div className=''>
+                <h3>{resData.name}</h3>
+                <h4>{resData.cuisines.join(', ')}</h4>
+                <h4>{resData.costForTwo}</h4>
+            </div>
         </div>
     )
 }
+
+const resObj = {
+    "id": "702769",
+    "name": "La Pino'z Pizza",
+    "cloudinaryImageId": "5fe4444a065b60c4fe39ef975222c12b",
+    "locality": "World One",
+    "areaName": "Sector 90",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+        "Pizzas",
+        "Pastas",
+        "Beverages",
+        "Fast Food",
+        "Italian",
+        "Desserts"
+    ],
+    "avgRating": 4.2,
+    "parentId": "4961",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1K+",
+    "promoted": true,
+    "adTrackingId": "cid=17608343~p=0~adgrpid=17608343#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=702769~eid=0733916d-043b-44be-b69f-83779d60614b~srvts=1724865993177~collid=45995",
+    "sla": {
+        "deliveryTime": 27,
+        "lastMileTravel": 3.9,
+        "serviceability": "SERVICEABLE",
+        "slaString": "25-30 mins",
+        "lastMileTravelString": "3.9 km",
+        "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+        "nextCloseTime": "2024-08-29 05:00:00",
+        "opened": true
+    },
+    "badges": {
+
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+        "entityBadges": {
+            "textBased": {
+
+            },
+            "imageBased": {
+
+            },
+            "textExtendedBadges": {
+
+            }
+        }
+    },
+    "aggregatedDiscountInfoV3": {
+        "header": "60% OFF",
+        "subHeader": "UPTO ₹120"
+    },
+    "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+
+            },
+            "video": {
+
+            }
+        }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+        "aggregatedRating": {
+            "rating": "--"
+        }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+}
+
 
 const Body = () => {
     return (
@@ -42,17 +127,7 @@ const Body = () => {
                 Search
             </div>
             <div className='res-container'>
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard resData={resObj} />
             </div>
         </div>
     )
