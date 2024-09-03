866f9348fccdb0c6ae0612fca663b59623548533
sagarkataria <sagarsingh0110@gmail.com>
2024-09-03 22:51:48 +0530

    'this state' in class based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,13 @@
+import { useState } from "react"
+
 const User = ({name}) => {
+    const {count1} = useState(0)
+    const {count2} = useState(1)
     return (
           <div>
+           
+            <h1>Count1: {count1}</h1>
+            <h1>Count2: {count2}</h1>
             <h1>{name}</h1>
             <h1>This is Namste React Series</h1>
           </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
-class UserClass extends React.Component{
+class UserClass extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+        }
     }
 
-    render(){
+    render() {
 
-        const {name,location} = this.props
-
-        return(
-               <div>
-                 <h2>Name : {name}</h2>
-                 <h3>Location :  {location}</h3>
-                 <h4>Contact: bila</h4>
-               </div>
+        const { name, location } = this.props;
+        const { count } = this.state;
+        return (
+            <div>
+                <h1>Count: {count}</h1>
+                <button
+                 onClick={()=>{
+                    this.setState({
+                        count:this.state.count+1
+                    })
+                 }}
+                >
+                Count Increase
+                </button>
+                <h2>Name : {name}</h2>
+                <h3>Location :  {location}</h3>
+                <h4>Contact: bila</h4>
+            </div>
         )
     }
 }
