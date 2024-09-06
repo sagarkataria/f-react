e6c0881da4d2ba76579cfa867d689ae9b53c5ad9
sagarkataria <sagarsingh0110@gmail.com>
2024-09-06 22:50:50 +0530

    make lazy loading

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
-import { createBrowserRouter,Outlet,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import About from './components/About.js';
 import ContactUs from './components/ContactUs.js';
 import Error from './components/Error.js';
 import RestaurantMenu from './components/RestaurantMenu.js';
 
 
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// lazy loading
+// on demand loading
+// dynamic import
+
+const Grocery = lazy(() => import('./components/Grocery'))
+const About = lazy(()=>import('./components/About'))
+
 const AppLayout = () => {
     return (
         <div className='app'>
             <Header />
-            <Outlet/>
+            <Outlet />
         </div>
     )
 }
 
 const appRouter = createBrowserRouter([
-   {
-    path : "/",
-    element : <AppLayout/>,
-    children:[
-        {
-            path:"/",
-            element:<Body/>
-        },
-        {
-            path:"/about",
-            element:<About/>
-        },
-        {
-            path:"/contact",
-            element:<ContactUs/>
-        },
-        {
-            path:"/restaurant/:resId",
-            element:<RestaurantMenu/>
-        }
-    ],
-    errorElement : <Error/>
-   },
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: (<Suspense fallback={<h1>Loading...</h1>}><About /></Suspense>)
+            },
+            {
+                path: "/contact",
+                element: <ContactUs />
+            },
+            {
+                path: "/grocery",
+                element: (<Suspense fallback={<h1>Loading...</h1>}><Grocery /></Suspense>)
+            },
+            {
+                path: "/restaurant/:resId",
+                element: <RestaurantMenu />
+            }
+        ],
+        errorElement: <Error />
+    },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/components/Grocery.js
@@ -0,0 +1,8 @@
+
+const Grocery = () => {
+  return (
+    <div>Grocery</div>
+  )
+}
+
+export default Grocery
