b7a4afc8837fd99f4de1fab3c7218a0021d44abd
sagarkataria <sagarsingh0110@gmail.com>
2024-09-08 12:53:53 +0530

    update context data by Provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
@@ -7,6 +7,7 @@ import About from './components/About.js';
 import ContactUs from './components/ContactUs.js';
 import Error from './components/Error.js';
 import RestaurantMenu from './components/RestaurantMenu.js';
+import UserContext from './utils/UserContext.js';
 
 
 // Chunking
@@ -17,14 +18,25 @@ import RestaurantMenu from './components/RestaurantMenu.js';
 // dynamic import
 
 const Grocery = lazy(() => import('./components/Grocery'))
-const About = lazy(()=>import('./components/About'))
+const About = lazy(() => import('./components/About'))
+
+
 
 const AppLayout = () => {
+    const [userName, setUserName] = useState();
+    useEffect(() => {
+        const data = {
+            name: "Sagar Singh",
+        }
+        setUserName(data.name)
+    }, [])
     return (
-        <div className='app'>
-            <Header />
-            <Outlet />
-        </div>
+        <UserContext.Provider value={{ loggedInUser: userName }}>
+            <div className='app'>
+                <Header />
+                <Outlet />
+            </div>
+        </UserContext.Provider>
     )
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import { Component } from "react"
 import User from "./User.js"
 import UserClass from "./UserClass.js"
+import UserContext from "../utils/UserContext.js";
 
 
 class About extends Component {
@@ -14,6 +15,12 @@ class About extends Component {
         return (
             <div className="about">
                 <h1>About Us</h1>
+                <div>
+                    Logged In User
+                    <UserContext.Consumer>
+                        {({ loggedInUser }) => <h1 className="font-bold">{loggedInUser}</h1>}
+                    </UserContext.Consumer>
+                </div>
                 <UserClass name={"Sagar Singh (Class)"} location={"Najibabad"} />
             </div>
         )
